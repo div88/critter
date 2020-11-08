4dfb4dd39933cc3b8d084d573999cfa02db24bf0
Divya Iyengar <divyaiyengar@divyas-mbp.attlocal.net>
2020-11-08 14:01:28 -0800

    Added getter setters for PEt

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -30,4 +30,43 @@ public class Pet implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
